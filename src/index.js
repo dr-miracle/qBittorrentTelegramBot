9e58ca12accff7fbe30e799a49c4fafefd710eff
Evgenij P. Miroshichenko <andy39@yandex.ru>
2020-11-16 15:35:34 +0300

    Add send messages

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 require('dotenv').config({path: __dirname + '/../config.env'});
 const { Telegraf } = require('telegraf');
-
 const bot = new Telegraf(process.env.TOKEN);
-bot.start((ctx) => ctx.reply('Welcome'))
-bot.command('oldschool', (ctx) => ctx.reply('Hello'))
-bot.command('modern', ({ reply }) => reply('Yo'))
-bot.command('hipster', Telegraf.reply('λ'))
 bot.launch()
+bot.telegram.sendMessage(process.env.CHATID, "Test message")
+    .then(() => {
+        process.exit(0);
+    })
+    .catch((e) => console.log(e));
+// bot.stop();
+// process.exit(0);
+// bot.use(session());
+// bot.reply("Fast reply!");
+// bot.start((ctx) => ctx.reply('Welcome'))
+// bot.context.reply("Fast reply!");
+// bot.command('oldschool', (ctx) => ctx.reply('Hello'))
+// bot.command('modern', ({ reply }) => reply('Yo'))
+// bot.command('hipster', Telegraf.reply('λ'))
+// bot.on('text', (ctx) => {
+//     ctx.session.counter = ctx.session.counter || 0
+//     ctx.session.counter++
+//     return ctx.reply(`Message counter:${ctx.session.counter}`)
+//   })
+
+// bot.stop();
