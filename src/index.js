772785e209c4484f02d1476ae0716346e2eea166
Evgenij P. Miroshichenko <andy39@yandex.ru>
2020-11-16 12:22:09 +0300

    Add telegraf example

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("Hello world!");
+require('dotenv').config({path: __dirname + '/../config.env'});
+const { Telegraf } = require('telegraf');
+
+const bot = new Telegraf(process.env.TOKEN);
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.command('oldschool', (ctx) => ctx.reply('Hello'))
+bot.command('modern', ({ reply }) => reply('Yo'))
+bot.command('hipster', Telegraf.reply('λ'))
+bot.launch()
