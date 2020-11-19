863f8661275a1146782683c0f51333f567c54c98
Evgenij P. Miroshichenko <andy39@yandex.ru>
2020-11-19 18:10:59 +0300

    Delete commented section

--- src/entity/user.js
+++ src/entity/user.js
@@ -1,21 +1,4 @@
 const { Model } = require('sequelize');
-exports.getUsers = () => 42;
-
-// class User extends Model {}
-// User.init({
-//     firstName: {
-//       type: DataTypes.STRING,
-//       allowNull: false
-//     },
-//     lastName: {
-//       type: DataTypes.STRING
-//       // allowNull defaults to true
-//     }
-//   }, {
-//     // Other model options go here
-//     sequelize, // We need to pass the connection instance
-//     modelName: 'User' // We need to choose the model name
-//   });
 
 exports.createModel = (sequelize, DataTypes) => {
     class User extends Model {}
