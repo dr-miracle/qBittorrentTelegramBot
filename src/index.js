73fc811eb258a6c608c0a3e4c657ea2235002916
Evgenij P. Miroshichenko <andy39@yandex.ru>
2020-11-22 14:21:06 +0300

    Add buttons example

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 require('dotenv').config({path: __dirname + '/../config.env'});
 const { Telegraf } = require('telegraf');
+const {MenuTemplate, MenuMiddleware} = require('telegraf-inline-menu')
 const { Database } = require("./db.js");
 
-const bot = new Telegraf(process.env.TOKEN);
+const getNonAuthUsersList = async (ctx) =>{
+    let users = await db.getAllUsers();
+    if (users.length === 0){
+        return ctx.reply("Список пользователей пуст!");
+    }
+    let userAuthList = users
+        .filter(u => !u.hasAuth)
+        .map(u => u.toString())
+        .join("\\n");
+    await ctx.reply("Список пользователей для одобрения:");
+    await ctx.reply(userAuthList);
+    return false;
+}
+
 const getUserData = (ctx) => {
     const userData = {
         userId: ctx.from.id,
@@ -12,17 +26,33 @@ const getUserData = (ctx) => {
     return userData;
 }
 const isAdmin = (userData) => userData.userId === process.env.ADMINID;
+
+const adminMenuTemplate = new MenuTemplate(ctx => `Hey ${ctx.from.first_name}!`);
+adminMenuTemplate.interact('NonAuthUsers', 'a', {
+    do: getNonAuthUsersList
+  });
+const menuMiddleware = new MenuMiddleware('/', adminMenuTemplate);
+
+const bot = new Telegraf(process.env.TOKEN);
+const db = new Database();
+bot.use(menuMiddleware.middleware());
 bot.on("document", (ctx) => {
     const isTorrentExtension = (filename) => {
         const fileExtension = filename.split(".").pop();
         return fileExtension === "torrent";
     }
     const filename = ctx.message.document.file_name;
-    if (isTorrentExtension(filename)){
-        ctx.reply("Add torrent!");
-    }else{
-        ctx.reply("It's not a torrent!");
+    if(!isTorrentExtension(filename)){
+        return;
     }
+    //show keyboard categories
+    //fs logic
+
+    // if (isTorrentExtension(filename)){
+    //     ctx.reply("Add torrent!");
+    // }else{
+    //     ctx.reply("It's not a torrent!");
+    // }
 })
 bot.start(async (ctx) => {
     if (ctx.from.is_bot){
@@ -41,20 +71,8 @@ bot.start(async (ctx) => {
     if (!isAdmin){
         return;
     }
-    let users = await db.getAllUsers();
-    if (users.length === 0){
-        return;
-    }
-    // console.log(users);
-    let userAuthList = users
-        .filter(u => !u.hasAuth)
-        .map(u => u.toString())
-        .join("\\n");
-    await ctx.reply("Список пользователей для одобрения:")
-    return ctx.reply(userAuthList);
+    menuMiddleware.replyToContext(ctx);
 });
-
-const db = new Database();
 
 const start = async() => {
     await db.init()
@@ -65,6 +83,8 @@ const start = async() => {
 start()
     .then(() => console.log("Bot started"))
     .catch((e) => console.log("Bot starting error: " + e));
+
+
 // bot.launch()
 // bot.telegram.sendMessage(process.env.CHATID, "Test message")
 //     .then(() => {
